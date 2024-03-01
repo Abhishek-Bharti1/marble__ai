a415cbd6cf2626a8339e2b927d7d2985f9bc2a31
Abhishek Bharti <abhishekbharti91550@gmail.com>
2024-03-01 11:33:14 +0530

    css fixed.

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -2,9 +2,9 @@ import { AiOutlineStock } from "react-icons/ai";
 import { GoQuestion } from "react-icons/go";
 const countryTemplate = (option) => {
     return (
-        <div className="flex align-items-center" style={{display:"flex",gap:"2rem",borderRadius:"0.5rem"}}>
+        <div className="flex align-items-center" style={{display:"flex",gap:"2rem",borderRadius:"0.5rem",justifyContent: "space-between"}}>
 <AiOutlineStock/>
-            <div>{option.name}</div>
+            <div style={{ flex: "1", textAlign: "start"}}>{option.name}</div>
             <GoQuestion />
         </div>
     );
